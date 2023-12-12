906eda020363b7995fac6a0367fd7836d2e61e72
yty-mac <1071567863@qq.com>
2023-12-12 23:32:52 +0800

    加入NodeFormer部分，目前设置达到82.4

--- pygcn/layers.py
+++ pygcn/layers.py
@@ -7,7 +7,7 @@ from torch.nn.parameter import Parameter
 from torch.nn.modules.module import Module
 import torch.nn.functional as F
 
-
+BIG_CONSTANT = 1e8
 class GraphConvolution(Module):
     """
     Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
@@ -42,6 +42,7 @@ class GraphConvolution(Module):
         return self.__class__.__name__ + ' (' \
             + str(self.in_features) + ' -> ' \
             + str(self.out_features) + ')'
+
 
 '''class Pool(Module):
     def __init__(self, in_feature, top_k_ratio, a_order):
@@ -139,7 +140,6 @@ class Upool(Module):
         return x, adj_zeros
 '''
 
-
 """自己加的CRF模块，用于构建经过gcn卷积得到的隐藏层之间相似的表示关系"""
 '''
 class CRF(Module):
@@ -196,13 +196,14 @@ class CRF(Module):
 
 '''
 
+
 class CRF_NN(Module):
     def __init__(self, in_dim, out_dim, num_iters, **kwargs):
         super(CRF_NN, self).__init__()
         self.in_dim = in_dim
         self.out_dim = out_dim
         self.num_iters = num_iters
-#        self.adj = adj
+        #        self.adj = adj
         self.alpha = nn.Parameter(torch.zeros(1))
         self.beta = nn.Parameter(torch.zeros(1))
         self.w_a = nn.Parameter(torch.nn.init.xavier_uniform_(torch.empty(in_dim, out_dim)))
@@ -217,11 +218,192 @@ class CRF_NN(Module):
         similarity = F.softmax(F.leaky_relu(logits), dim=1)
         gi = torch.sum(similarity, dim=1, keepdim=True)
         gi = F.normalize(gi)
-        print(gi.shape)
+        #print(gi.shape)
 
         alpha = torch.exp(self.alpha)
         beta = torch.exp(self.beta)
         for i in range(self.num_iters):
             output = (alpha * x + beta * gi * output)
             output = output / (alpha + beta * gi)
+        print(alpha)
+        print(beta)
         return output
+
+def create_projection_matrix(m, d, seed=0, scaling=0, struct_mode=False):
+    nb_full_blocks = int(m / d)
+    block_list = []
+    current_seed = seed
+    for _ in range(nb_full_blocks):
+        torch.manual_seed(current_seed)
+        if struct_mode:
+            q = create_products_of_givens_rotations(d, current_seed)
+        else:
+            unstructured_block = torch.randn((d, d))
+            q, _ = torch.linalg.qr(unstructured_block)
+            q = torch.t(q)
+        block_list.append(q)
+        current_seed += 1
+    remaining_rows = m - nb_full_blocks * d
+    if remaining_rows > 0:
+        torch.manual_seed(current_seed)
+        if struct_mode:
+            q = create_products_of_givens_rotations(d, current_seed)
+        else:
+            unstructured_block = torch.randn((d, d))
+            q, _ = torch.linalg.qr(unstructured_block)
+            q = torch.t(q)
+        block_list.append(q[0:remaining_rows])
+    final_matrix = torch.vstack(block_list)
+
+    current_seed += 1
+    torch.manual_seed(current_seed)
+    if scaling == 0:
+        multiplier = torch.norm(torch.randn((m, d)), dim=1)
+    elif scaling == 1:
+        multiplier = torch.sqrt(torch.tensor(float(d))) * torch.ones(m)
+    else:
+        raise ValueError("Scaling must be one of {0, 1}. Was %s" % scaling)
+
+    return torch.matmul(torch.diag(multiplier), final_matrix)
+
+
+def softmax_kernel_transformation(data, is_query, projection_matrix=None, numerical_stabilizer=0.000001):
+    data_normalizer = 1.0 / torch.sqrt(torch.sqrt(torch.tensor(data.shape[-1], dtype=torch.float32)))
+    data = data_normalizer * data
+    ratio = 1.0 / torch.sqrt(torch.tensor(projection_matrix.shape[0], dtype=torch.float32))  # 1/√m
+    data_dash = torch.einsum("bnhd,md->bnhm", data, projection_matrix)
+    diag_data = torch.square(data)
+    diag_data = torch.sum(diag_data, dim=len(data.shape) - 1)
+    diag_data = diag_data / 2.0
+    diag_data = torch.unsqueeze(diag_data, dim=len(data.shape) - 1)
+    last_dims_t = len(data_dash.shape) - 1
+    attention_dims_t = len(data_dash.shape) - 3
+    if is_query:
+        data_dash = ratio * (
+                torch.exp(data_dash - diag_data - torch.max(data_dash, dim=last_dims_t, keepdim=True)[
+                    0]) + numerical_stabilizer
+        )
+    else:
+        data_dash = ratio * (
+                torch.exp(data_dash - diag_data - torch.max(torch.max(data_dash, dim=last_dims_t, keepdim=True)[0],
+                                                            dim=attention_dims_t, keepdim=True)[
+                    0]) + numerical_stabilizer
+        )
+    return data_dash
+
+def kernelized_softmax(query, key, value, kernel_transformation, projection_matrix=None, edge_index=None, tau=0.25, return_weight=False):
+    '''
+    fast computation of all-pair attentive aggregation with linear complexity
+    input: query/key/value [B, N, H, D]
+    return: updated node emb, attention weight (for computing edge loss)
+    B = graph number (always equal to 1 in Node Classification), N = node number, H = head number,
+    M = random feature dimension, D = hidden size
+    '''
+    query = query / math.sqrt(tau)
+    key = key / math.sqrt(tau)
+    query_prime = kernel_transformation(query, True, projection_matrix) # [B, N, H, M]
+    key_prime = kernel_transformation(key, False, projection_matrix) # [B, N, H, M]
+    query_prime = query_prime.permute(1, 0, 2, 3) # [N, B, H, M]
+    key_prime = key_prime.permute(1, 0, 2, 3) # [N, B, H, M]
+    value = value.permute(1, 0, 2, 3) # [N, B, H, D]
+
+    # compute updated node emb, this step requires O(N)
+    z_num = numerator(query_prime, key_prime, value)
+    z_den = denominator(query_prime, key_prime)
+
+    z_num = z_num.permute(1, 0, 2, 3)  # [B, N, H, D]
+    z_den = z_den.permute(1, 0, 2)
+    z_den = torch.unsqueeze(z_den, len(z_den.shape))
+    z_output = z_num / z_den # [B, N, H, D]
+
+    if return_weight: # query edge prob for computing edge-level reg loss, this step requires O(E)
+        start, end = edge_index
+        query_end, key_start = query_prime[end], key_prime[start] # [E, B, H, M]
+        edge_attn_num = torch.einsum("ebhm,ebhm->ebh", query_end, key_start) # [E, B, H]
+        edge_attn_num = edge_attn_num.permute(1, 0, 2) # [B, E, H]
+        attn_normalizer = denominator(query_prime, key_prime) # [N, B, H]
+        edge_attn_dem = attn_normalizer[end]  # [E, B, H]
+        edge_attn_dem = edge_attn_dem.permute(1, 0, 2) # [B, E, H]
+        A_weight = edge_attn_num / edge_attn_dem # [B, E, H]
+
+        return z_output, A_weight
+
+    else:
+        return z_output,z_den
+
+
+def numerator(qs, ks, vs):
+    kvs = torch.einsum("nbhm,nbhd->bhmd", ks, vs) # kvs refers to U_k in the paper
+    return torch.einsum("nbhm,bhmd->nbhd", qs, kvs)
+
+def denominator(qs, ks):
+    all_ones = torch.ones([ks.shape[0]]).to(qs.device)
+    ks_sum = torch.einsum("nbhm,n->bhm", ks, all_ones) # ks_sum refers to O_k in the paper
+    return torch.einsum("nbhm,bhm->nbh", qs, ks_sum)
+
+
+
+class CRF_Node(Module):
+    def __init__(self, in_dim, out_dim, hidden=128, num_iters=2,num_heads=4,kernel_transformation=softmax_kernel_transformation,
+                 projection_matrix_type='a',random_features=64, **kwargs):
+        super(CRF_Node, self).__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.hidden = hidden
+        self.num_iters = num_iters
+        #        self.adj = adj
+        self.alpha = nn.Parameter(torch.zeros(1))
+        self.beta = nn.Parameter(torch.zeros(1))
+        self.num_heads = num_heads
+        self.Wq = nn.Linear(in_dim,num_heads*hidden)
+        self.Wk = nn.Linear(in_dim,num_heads*hidden)
+        self.Wv = nn.Linear(in_dim,num_heads*hidden)
+        self.Wo = nn.Linear(num_heads*hidden,out_dim)
+        self.kernel_transformation = kernel_transformation
+        self.projection_matrix_type = projection_matrix_type
+        self.random_features = random_features
+
+    def reset_parameters(self):
+        self.Wq.reset_parameters()
+        self.Wk.reset_parameters()
+        self.Wv.reset_parameters()
+        self.Wo.reset_parameters()
+
+    def forward(self, x, adj, tau=0.09):
+        output = x
+        N,D = x.size(0),x.size(1)
+        query = self.Wq(x).reshape(-1,N,self.num_heads,self.hidden)
+        key = self.Wk(x).reshape(-1,N,self.num_heads,self.hidden)
+        value = self.Wv(x).reshape(-1,N,self.num_heads,self.hidden)
+
+        seed = torch.ceil(torch.abs(torch.sum(query) * BIG_CONSTANT)).to(torch.int32)
+        projection_matrix = create_projection_matrix(self.random_features,self.hidden,seed=seed)
+
+        x_next, x_den = kernelized_softmax(query, key, value, self.kernel_transformation, projection_matrix, adj, tau)
+        x_next = self.Wo(x_next.flatten(-2, -1)).squeeze(0) # 1x N x (h*d) -> Nxout_dim
+        #print(x_den.shape)
+        #x_den = x_den.flatten(-2,-1).squeeze(0)
+        #x_den = self.alpha + self.beta * x_den
+        alpha = torch.exp(self.alpha)
+        beta = torch.exp(self.beta)
+        for i in range(self.num_iters):
+            output = (alpha * output + beta * x_next)
+        print(alpha)
+        print(beta)
+
+        '''x1 = torch.matmul(x, self.w_a)
+        x2 = torch.matmul(x, self.w_b)
+
+        logits = torch.matmul(x1, x2.t())
+        similarity = F.softmax(F.leaky_relu(logits), dim=1)
+        gi = torch.sum(similarity, dim=1, keepdim=True)
+        gi = F.normalize(gi)
+        print(gi.shape)
+
+        alpha = torch.exp(self.alpha)
+        beta = torch.exp(self.beta)
+        for i in range(self.num_iters):
+            output = (alpha * x + beta * gi * output)
+            output = output / (alpha + beta * gi)'''
+        return output
+
--- pygcn/models.py
+++ pygcn/models.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from pygcn.layers import GraphConvolution,  CRF_NN#,CRF, Pool, Upool
+from pygcn.layers import GraphConvolution,  CRF_NN, CRF_Node#,CRF, Pool, Upool
 
 
 class GCN(nn.Module):
@@ -15,12 +15,16 @@ class GCN(nn.Module):
         #self.upool = Upool(nhid,0.6)
         self.dropout = dropout
         self.crf_nn = CRF_NN(nhid,nhid,2)
+        self.crf_node = CRF_Node(nhid, nhid,3)
+        self.layer_norm = nn.LayerNorm(nhid)
 
     def forward(self, x, adj):
         n = x.size(0)
         x = F.relu(self.gc1(x, adj))
 
-        x = self.crf_nn(x)
+        #x = self.crf_nn(x)
+        x = self.crf_node(x,adj)
+        x = self.layer_norm(x)
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, adj)
 
